1fb7e63e5e74608c3d70ecceaeed09ef39c603f9
JokinCG <j.calventegalan@gmail.com>
2016-09-21 14:31:48 +0200

    Modificamos capa presentación para incluir un menú con las distintas opciones

--- principal/Presentacion.java
+++ principal/Presentacion.java
@@ -1,3 +1,4 @@
+
 package principal;
 
 import java.util.Scanner;
@@ -11,22 +12,42 @@ public class Presentacion {
         int a, b;
         Scanner sc = new Scanner(System.in);
         
-                
-        System.out.print("Introduce el número 1: ");
-        a = sc.nextInt();
-        System.out.print("Introduce el número 2: ");
-        b = sc.nextInt();
+        String operacion;
         
+        do{
+            System.out.print("Introduce el número 1: ");
+            a = sc.nextInt();
+            System.out.print("Introduce el número 2: ");
+            b = sc.nextInt();
+
+
+            Calculadora cal = new Calculadora(a, b);
+
+
+            cal.fijarNum1(a);
+            cal.fijarNum2(b);
+            System.out.println("¿Qué operación desea realizar? sumar / restar/ multiplicar");
+            operacion = sc.nextLine();        
+
+            switch (operacion){
+             case "sumar":
+                 System.out.println("Suma: " + cal.sumar());
+                 break;
+             case "restar":
+                 System.out.println("Resta: " + cal.restar());
+                 break;
+             case "multiplicar":
+                 System.out.println("Producto: " + cal.multiplicar());
+                 break;
+             default:
+                 System.out.println("Opción No disponible");
+                 break;
+            }
         
-        Calculadora cal = new Calculadora(a, b);
-        
-               
-        cal.fijarNum1(a);
-        cal.fijarNum2(b);
-        
-   
-        System.out.println("Suma: " + cal.sumar());
-        System.out.println("Resta: " + cal.restar());
-        System.out.println("Producto: " + cal.multiplicar());
+        }while (operacion != "salir");
+    
     }
 }
+        
+    
+    
