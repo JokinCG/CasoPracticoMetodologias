5c8387d3975a0fab02a31a225ba2cad5e5216d61
JokinCG <j.calventegalan@gmail.com>
2016-09-21 14:07:17 +0200

    Modificamos capa presentación para ejecutar función suma

--- principal/Presentacion.java
+++ principal/Presentacion.java
@@ -1,12 +1,30 @@
-
 package principal;
+
+import java.util.Scanner;
+import modelo.Calculadora;
 
 
 public class Presentacion {
 
     
     public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
         
+                
+        System.out.print("Introduce el número 1: ");
+        a = sc.nextInt();
+        System.out.print("Introduce el número 2: ");
+        b = sc.nextInt();
+        
+        
+        Calculadora cal = new Calculadora(a, b);
+        
+               
+        cal.fijarNum1(a);
+        cal.fijarNum2(b);
+        
+   
+        System.out.println("Suma: " + cal.sumar());
     }
-    
 }
