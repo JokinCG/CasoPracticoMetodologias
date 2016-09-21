b7bfb325e5b936544c9c022f0e083be4f050ff2d
JokinCG <j.calventegalan@gmail.com>
2016-09-21 13:23:35 +0200

    Creado proyecto java Calculadora

---
+++ modelo/Calculadora.java
@@ -0,0 +1,24 @@
+
+package modelo;
+
+public class Calculadora {
+    
+    //Creamos las variables tipo entero
+    
+    int num1, num2;
+ 
+    //Constructor que sirve para inicializar los atributos num1 y num2
+    public Calculadora(int a, int b){
+        num1 = a;
+        num2 = b;
+    }
+        
+    public void fijarNum1(int n){
+        num1=n;
+    }
+    public void fijarNum2(int n){
+        num2=n;
+    }
+    
+    
+}
---
+++ principal/Presentacion.java
@@ -0,0 +1,12 @@
+
+package principal;
+
+
+public class Presentacion {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
